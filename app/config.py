0f64c4294e7e7d610bfeccd9d51b35e4ec62864f
ShirsatTanvi <shirsattanvi2201@gmail.com>
2025-07-29 19:17:15 +0530

    Project Completed

--- app/config.py
+++ app/config.py
@@ -2,5 +2,6 @@ import os
 
 class Settings:
     DATABASE_URL = os.getenv("DATABASE_URL", "mysql+pymysql://root:tanvi.2002@localhost/ecommerce_db")
+    # DATABASE_URL = "mysql+pymysql://root:tanvi.2002@host.docker.internal:3306/ecommerce_db"
     
 settings = Settings()
